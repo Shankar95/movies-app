6431b8ae550f5af8e82e014f910a7606e3e94efd
Shankar95 <shankaran.sunderarajan@byjus.com>
2022-03-02 18:33:37 +0530

    commit all the codes to movies app

---
+++ movies-app/client/src/constants/movies.js
@@ -0,0 +1,51 @@
+const movies = [{
+    id: 1,
+    title: 'Superman',
+    poster: '',
+    rating: 8.6
+}, {
+    id: 2,
+    title: 'Spiderman',
+    poster: '',
+    rating: 9
+}, {
+    id: 3,
+    title: 'Ironman',
+    poster: '',
+    rating: 7.8
+}, {
+    id: 4,
+    title: 'Batman',
+    poster: '',
+    rating: 10
+}, {
+    id: 5,
+    title: 'Wonder Woman',
+    poster: '',
+    rating: 9
+}, {
+    id: 6,
+    title: 'FF7',
+    poster: '',
+    rating: 10
+}, {
+    id: 7,
+    title: 'Harry parter 1',
+    poster: '',
+    rating: 8.8
+}, {
+    id: 8,
+    title: 'Pursuit of happiness',
+    poster: '',
+    rating: 8.2
+}, {
+    id: 9,
+    title: 'Titanic',
+    poster: '',
+    rating: 8.5
+ }];
+
+
+export {
+    movies
+}
